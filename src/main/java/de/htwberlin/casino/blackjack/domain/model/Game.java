6c4995f00f17613e2dab5aadaf8b2315867b731b
voelkerh <henriette.voelker@posteo.de>
2025-06-21 19:13:35 +0200

    Make Game an Entity with persistence id

--- src/main/java/de/htwberlin/casino/blackjack/domain/model/Game.java
+++ src/main/java/de/htwberlin/casino/blackjack/domain/model/Game.java
@@ -1,4 +1,12 @@
 package de.htwberlin.casino.blackjack.domain.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.util.UUID;
+
+@Entity
 public class Game {
+    @Id
+    private UUID id;
 }
