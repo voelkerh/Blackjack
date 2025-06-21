4f0bba0202e0c3b10eaa56750033d57b9e90af9c
voelkerh <henriette.voelker@posteo.de>
2025-06-21 19:11:19 +0200

    Setup calculateChances Interface and Service class with Service Tag

--- src/main/java/de/htwberlin/casino/blackjack/application/chances/CalculateChancesService.java
+++ src/main/java/de/htwberlin/casino/blackjack/application/chances/CalculateChancesService.java
@@ -1,6 +1,15 @@
 package de.htwberlin.casino.blackjack.application.chances;
 
 import de.htwberlin.casino.blackjack.ports.inbound.CalculateChancesUseCase;
+import de.htwberlin.casino.blackjack.utility.ErrorWrapper;
+import de.htwberlin.casino.blackjack.utility.Result;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CalculateChancesService implements CalculateChancesUseCase {
+
+    @Override
+    public Result<String, ErrorWrapper> calculateChances(int chancesRequest) {
+        return null;
+    }
 }
--- src/main/java/de/htwberlin/casino/blackjack/ports/inbound/CalculateChancesUseCase.java
+++ src/main/java/de/htwberlin/casino/blackjack/ports/inbound/CalculateChancesUseCase.java
@@ -1,4 +1,8 @@
 package de.htwberlin.casino.blackjack.ports.inbound;
 
+import de.htwberlin.casino.blackjack.utility.ErrorWrapper;
+import de.htwberlin.casino.blackjack.utility.Result;
+
 public interface CalculateChancesUseCase {
+    Result<String, ErrorWrapper> calculateChances(int gameId);
 }
