659f56df2158f9f164440cf020f69e09c6ff1993
voelkerh <henriette.voelker@posteo.de>
2025-06-21 19:10:16 +0200

    Create dummy test class to make project mvn buildable

---
+++ src/test/java/de/htwberlin/casino/blackjack/application/chances/CalculateChancesServiceTest.java
@@ -0,0 +1,12 @@
+package de.htwberlin.casino.blackjack.application.chances;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculateChancesServiceTest {
+
+    @Test
+    void calculateChances() {
+    }
+}
