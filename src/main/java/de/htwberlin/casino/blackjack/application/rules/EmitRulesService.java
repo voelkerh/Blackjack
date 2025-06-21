91e5ac1eaf0583ee518387087d6dd0965a66b4ed
voelkerh <henriette.voelker@posteo.de>
2025-06-21 19:11:45 +0200

    Setup emitRules Interface and Service class with Service Tag

--- src/main/java/de/htwberlin/casino/blackjack/application/rules/EmitRulesService.java
+++ src/main/java/de/htwberlin/casino/blackjack/application/rules/EmitRulesService.java
@@ -1,6 +1,20 @@
 package de.htwberlin.casino.blackjack.application.rules;
 
 import de.htwberlin.casino.blackjack.ports.inbound.EmitRulesUseCase;
+import de.htwberlin.casino.blackjack.utility.ErrorWrapper;
+import de.htwberlin.casino.blackjack.utility.Result;
+import org.springframework.stereotype.Service;
 
+@Service
 public class EmitRulesService implements EmitRulesUseCase {
+
+    @Override
+    public Result getRules() {
+        return null;
+    }
+
+    @Override
+    public Result<String, ErrorWrapper> getRules(String action) {
+        return null;
+    }
 }
