19022ed8250a2a149933cba266612225ca36c02d
voelkerh <henriette.voelker@posteo.de>
2025-06-21 19:12:39 +0200

    Add Service tags to Service classes

--- src/main/java/de/htwberlin/casino/blackjack/application/stats/EmitStatsService.java
+++ src/main/java/de/htwberlin/casino/blackjack/application/stats/EmitStatsService.java
@@ -1,6 +1,8 @@
 package de.htwberlin.casino.blackjack.application.stats;
 
 import de.htwberlin.casino.blackjack.ports.inbound.EmitStatsUseCase;
+import org.springframework.stereotype.Service;
 
+@Service
 public class EmitStatsService implements EmitStatsUseCase {
 }
