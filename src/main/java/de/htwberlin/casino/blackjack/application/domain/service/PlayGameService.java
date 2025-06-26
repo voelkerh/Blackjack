d98d8361c6383e77d4be9deef1394cff9a628137
voelkerh <henriette.voelker@posteo.de>
2025-06-26 13:38:55 +0200

    Move and declare Service classes

---
+++ src/main/java/de/htwberlin/casino/blackjack/application/domain/service/PlayGameService.java
@@ -0,0 +1,8 @@
+package de.htwberlin.casino.blackjack.application.domain.service;
+
+import de.htwberlin.casino.blackjack.application.port.in.playGame.PlayGameUseCase;
+import org.springframework.stereotype.Service;
+
+@Service
+class PlayGameService implements PlayGameUseCase {
+}
