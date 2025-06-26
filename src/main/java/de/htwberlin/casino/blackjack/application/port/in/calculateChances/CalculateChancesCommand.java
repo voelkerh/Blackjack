13b7f7120815442f552a4e2ff6c17b1a97070b92
voelkerh <henriette.voelker@posteo.de>
2025-06-26 13:34:55 +0200

    Add query and command record classes

---
+++ src/main/java/de/htwberlin/casino/blackjack/application/port/in/calculateChances/CalculateChancesCommand.java
@@ -0,0 +1,4 @@
+package de.htwberlin.casino.blackjack.application.port.in.calculateChances;
+
+public record CalculateChancesCommand(int gameId) {
+}
---
+++ src/main/java/de/htwberlin/casino/blackjack/application/port/in/emitRules/EmitRulesQuery.java
@@ -0,0 +1,6 @@
+package de.htwberlin.casino.blackjack.application.port.in.emitRules;
+
+import de.htwberlin.casino.blackjack.application.domain.model.RuleOption;
+
+public record EmitRulesQuery(RuleOption option) {
+}
