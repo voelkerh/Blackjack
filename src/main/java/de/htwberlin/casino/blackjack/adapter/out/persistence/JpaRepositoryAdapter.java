145b3d1ce47c4d7f2924ae652f280b626d224764
voelkerh <henriette.voelker@posteo.de>
2025-07-03 11:18:18 +0200

    Implement database access in Repository Adapter

---
+++ src/main/java/de/htwberlin/casino/blackjack/adapter/out/persistence/JpaRepositoryAdapter.java
@@ -0,0 +1,27 @@
+package de.htwberlin.casino.blackjack.adapter.out.persistence;
+
+import de.htwberlin.casino.blackjack.application.domain.model.RuleOption;
+import de.htwberlin.casino.blackjack.application.domain.model.Rules;
+import de.htwberlin.casino.blackjack.application.port.out.LoadGameStatePort;
+import de.htwberlin.casino.blackjack.application.port.out.LoadRulesPort;
+import de.htwberlin.casino.blackjack.application.port.out.LoadStatsPort;
+import de.htwberlin.casino.blackjack.application.port.out.ModifyGameStatePort;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@RequiredArgsConstructor
+@Repository
+class JpaRepositoryAdapter implements LoadRulesPort, LoadStatsPort, LoadGameStatePort, ModifyGameStatePort {
+
+    private final JpaGameRepository gameRepository;
+    private final JpaRulesRepository rulesRepository;
+    private final GameMapper gameMapper;
+    private final RulesMapper rulesMapper;
+
+    @Override
+    public Rules retrieveRules(RuleOption option) {
+        RulesJpaEntity rulesJpaEntity = rulesRepository.findById(option.toString()).orElseThrow(EntityNotFoundException::new);
+        return rulesMapper.mapToDomainEntity(RuleOption.valueOf(rulesJpaEntity.getOption()), rulesJpaEntity.getRules());
+    }
+}
