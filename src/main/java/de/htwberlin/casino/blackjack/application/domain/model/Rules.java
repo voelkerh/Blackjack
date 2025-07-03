0dcd8310230b0945021bbaa2523e05b140a4e692
voelkerh <henriette.voelker@posteo.de>
2025-07-03 11:17:18 +0200

    Make domain rules record class

--- src/main/java/de/htwberlin/casino/blackjack/application/domain/model/Rules.java
+++ src/main/java/de/htwberlin/casino/blackjack/application/domain/model/Rules.java
@@ -1,15 +1,4 @@
 package de.htwberlin.casino.blackjack.application.domain.model;
 
-import lombok.*;
-
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class Rules {
-
-    private Long id;
-    private String option;
-    private String rule;
-
+public record Rules (RuleOption option, String rulesText) {
 }
