e42fa2ed2429008afa13e5b3e91584035fba17b8
Joh <132756126+JohPy@users.noreply.github.com>
2025-07-05 17:26:26 +0200

    Define EmitStatsUseCase interface for emitting stats
    * also provide placeholder EmitStatsService implementation of EmitStatsUseCase

--- src/main/java/de/htwberlin/casino/blackjack/application/domain/service/EmitStatsService.java
+++ src/main/java/de/htwberlin/casino/blackjack/application/domain/service/EmitStatsService.java
@@ -1,8 +1,15 @@
 package de.htwberlin.casino.blackjack.application.domain.service;
 
+import de.htwberlin.casino.blackjack.application.port.in.emitStats.EmitStatsQuery;
 import de.htwberlin.casino.blackjack.application.port.in.emitStats.EmitStatsUseCase;
+import de.htwberlin.casino.blackjack.utility.ErrorWrapper;
+import de.htwberlin.casino.blackjack.utility.Result;
 import org.springframework.stereotype.Service;
 
 @Service
 class EmitStatsService implements EmitStatsUseCase {
+    @Override
+    public Result<String, ErrorWrapper> emitStats(EmitStatsQuery query) {
+        return null;
+    }
 }
--- src/main/java/de/htwberlin/casino/blackjack/application/port/in/emitStats/EmitStatsUseCase.java
+++ src/main/java/de/htwberlin/casino/blackjack/application/port/in/emitStats/EmitStatsUseCase.java
@@ -1,4 +1,8 @@
 package de.htwberlin.casino.blackjack.application.port.in.emitStats;
 
+import de.htwberlin.casino.blackjack.utility.ErrorWrapper;
+import de.htwberlin.casino.blackjack.utility.Result;
+
 public interface EmitStatsUseCase {
+    Result<String, ErrorWrapper> emitStats(EmitStatsQuery query);
 }
