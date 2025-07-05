a6274330642400a31195fb86df85068c7898d5c9
voelkerh <henriette.voelker@posteo.de>
2025-07-05 13:54:31 +0200

    Add Getter to PlayerHand

--- src/main/java/de/htwberlin/casino/blackjack/application/domain/model/PlayerHand.java
+++ src/main/java/de/htwberlin/casino/blackjack/application/domain/model/PlayerHand.java
@@ -1,8 +1,11 @@
 package de.htwberlin.casino.blackjack.application.domain.model;
 
+import lombok.Getter;
+
 import java.util.Arrays;
 import java.util.List;
 
+@Getter
 public class PlayerHand {
 
     private final List<Card> cards;
@@ -10,4 +13,5 @@ public class PlayerHand {
     public PlayerHand(Card card1, Card card2) {
         this.cards = Arrays.asList(card1, card2);
     }
+
 }
