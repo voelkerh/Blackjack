785f507b5707fa8640285082f888939606ba5d2a
voelkerh <henriette.voelker@posteo.de>
2025-07-05 13:51:44 +0200

    Define LoadGameStatePort

--- src/main/java/de/htwberlin/casino/blackjack/adapter/out/persistence/JpaGameRepositoryAdapter.java
+++ src/main/java/de/htwberlin/casino/blackjack/adapter/out/persistence/JpaGameRepositoryAdapter.java
@@ -1,18 +1,24 @@
 package de.htwberlin.casino.blackjack.adapter.out.persistence;
 
+import de.htwberlin.casino.blackjack.application.domain.model.Game;
 import de.htwberlin.casino.blackjack.application.domain.model.RuleOption;
 import de.htwberlin.casino.blackjack.application.domain.model.Rules;
-import de.htwberlin.casino.blackjack.application.port.out.LoadGameStatePort;
+import de.htwberlin.casino.blackjack.application.port.out.LoadGamePort;
 import de.htwberlin.casino.blackjack.application.port.out.LoadRulesPort;
 import de.htwberlin.casino.blackjack.application.port.out.LoadStatsPort;
 import de.htwberlin.casino.blackjack.application.port.out.ModifyGameStatePort;
 import org.springframework.stereotype.Repository;
 
 @Repository
-class JpaGameRepositoryAdapter implements LoadRulesPort, LoadStatsPort, LoadGameStatePort, ModifyGameStatePort {
+class JpaGameRepositoryAdapter implements LoadRulesPort, LoadStatsPort, LoadGamePort, ModifyGameStatePort {
 
     @Override
     public Rules retrieveRules(RuleOption option) {
+        return null;
+    }
+
+    @Override
+    public Game retrieveGame(int gameId) {
         return null;
     }
 }
