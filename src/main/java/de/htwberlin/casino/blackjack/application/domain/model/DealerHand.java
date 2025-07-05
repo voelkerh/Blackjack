633e98484ab5287c6eab4ce9d7fb2e13b25cd3ec
voelkerh <henriette.voelker@posteo.de>
2025-07-05 13:48:50 +0200

    Setup DealerHand and PlayerHand

--- src/main/java/de/htwberlin/casino/blackjack/application/domain/model/DealerHand.java
+++ src/main/java/de/htwberlin/casino/blackjack/application/domain/model/DealerHand.java
@@ -1,4 +1,14 @@
 package de.htwberlin.casino.blackjack.application.domain.model;
 
+import lombok.Getter;
+
+@Getter
 public class DealerHand {
+
+    private final Card upCard;
+
+    public DealerHand(Card upCard) {
+        this.upCard = upCard;
+    }
+
 }
--- src/main/java/de/htwberlin/casino/blackjack/application/domain/model/PlayerHand.java
+++ src/main/java/de/htwberlin/casino/blackjack/application/domain/model/PlayerHand.java
@@ -1,4 +1,13 @@
 package de.htwberlin.casino.blackjack.application.domain.model;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class PlayerHand {
+
+    private final List<Card> cards;
+
+    public PlayerHand(Card card1, Card card2) {
+        this.cards = Arrays.asList(card1, card2);
+    }
 }
