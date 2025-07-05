83d08f0f86bec7bd8b5d9f38b5602d0c7b17b0c6
voelkerh <henriette.voelker@posteo.de>
2025-07-05 13:00:10 +0200

    Create CardDeck as Singleton based on Card class

--- src/main/java/de/htwberlin/casino/blackjack/application/domain/model/CardDeck.java
+++ src/main/java/de/htwberlin/casino/blackjack/application/domain/model/CardDeck.java
@@ -1,4 +1,37 @@
 package de.htwberlin.casino.blackjack.application.domain.model;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import static java.util.Collections.shuffle;
+
 public class CardDeck {
+
+    private static CardDeck instance;
+    private final List<Card> deck;
+
+    private CardDeck() {
+        deck = initializeDeck();
+        shuffle(deck);
+    }
+
+    public static CardDeck getInstance(){
+        if(instance == null) instance = new CardDeck();
+        return instance;
+    }
+
+    private List<Card> initializeDeck() {
+        List<Card> deck = new LinkedList<>();
+        for (Suit suit : Suit.values()) {
+            for (Rank rank : Rank.values()) {
+                deck.add(new Card(rank, suit));
+            }
+        }
+        return deck;
+    }
+
+    public Card drawCard() {
+        return deck.removeFirst();
+    }
+
 }
