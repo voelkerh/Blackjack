2405aa1e80be1da7c5a2a0b560b20201b5b3fe23
voelkerh <henriette.voelker@posteo.de>
2025-07-12 20:30:26 +0200

    Refactor ChancesCalculatorImpl with ace handling

--- src/main/java/de/htwberlin/casino/blackjack/application/domain/service/calculateChances/ChancesCalculatorImpl.java
+++ src/main/java/de/htwberlin/casino/blackjack/application/domain/service/calculateChances/ChancesCalculatorImpl.java
@@ -1,8 +1,6 @@
 package de.htwberlin.casino.blackjack.application.domain.service.calculateChances;
 
-import de.htwberlin.casino.blackjack.application.domain.model.Card;
-import de.htwberlin.casino.blackjack.application.domain.model.DealerHand;
-import de.htwberlin.casino.blackjack.application.domain.model.PlayerHand;
+import de.htwberlin.casino.blackjack.application.domain.model.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -20,61 +18,62 @@ public class ChancesCalculatorImpl implements ChancesCalculator {
     private final int VALUE_TEN_CARDS_IN_CARD_DECK = 16;
 
     @Override
-    public Chances calculateChances(PlayerHand playerHand, DealerHand dealerHand) {
-        List<Card> playerCards = playerHand.getCards();
-        List<Card> dealerCards = dealerHand.getCards();
-        int differenceToBlackjack = getDifferenceToBlackjack(playerCards);
-        double blackjackChance = calculateBlackjackChance(differenceToBlackjack, playerCards, dealerCards);
-        double bustChance = calculateBustChance(differenceToBlackjack, playerCards, dealerCards);
+    public Chances calculateChances(Hand playerHand, Hand dealerHand) {
+        int differenceToBlackjack = getDifferenceToBlackjack(playerHand);
+        double blackjackChance = calculateBlackjackChance(differenceToBlackjack, playerHand, dealerHand);
+        double bustChance = calculateBustChance(differenceToBlackjack, playerHand, dealerHand);
         return new Chances(bustChance, blackjackChance);
     }
 
-    private int getDifferenceToBlackjack(List<Card> playerCards) {
-        return BLACKJACK_VALUE - getPlayerHandTotal(playerCards);
+    private int getDifferenceToBlackjack(Hand playerHand) {
+        return BLACKJACK_VALUE - playerHand.getTotal();
     }
 
-    private double calculateBlackjackChance(int differenceToBlackjack, List<Card> playerCards, List<Card> dealerCards) {
-        if (differenceToBlackjack != 10) return calculateValueChanceNotTen(differenceToBlackjack, playerCards, dealerCards);
-        else return calculateValueChanceTen(playerCards, dealerCards);
+    private double calculateBlackjackChance(int differenceToBlackjack, Hand playerHand, Hand dealerHand) {
+        if (differenceToBlackjack != 10) return calculateValueChanceNotTen(differenceToBlackjack, playerHand, dealerHand);
+        else return calculateValueChanceTen(playerHand, dealerHand);
     }
 
-    private double calculateValueChanceNotTen(int differenceToBlackjack, List<Card> playerCards, List<Card> dealerCards) {
-        int totalNumberOfCardsDealt = getTotalNumberOfCardsDealt(playerCards, dealerCards);
-        int numberOfValueCardsDealt = getNumberOfValueCardsDealt(differenceToBlackjack, playerCards, dealerCards);
+    private double calculateValueChanceNotTen(int differenceToBlackjack, Hand playerHand, Hand dealerHand) {
+        int totalNumberOfCardsDealt = getTotalNumberOfCardsDealt(playerHand, dealerHand);
+        int numberOfValueCardsDealt = getNumberOfValueCardsDealt(differenceToBlackjack, playerHand, dealerHand);
         return (double) ((VALUE_CARDS_IN_CARD_DECK * NUMBER_OF_CARD_DECKS) - numberOfValueCardsDealt) /
                 (NUMBER_OF_CARDS_IN_CARD_DECK * NUMBER_OF_CARD_DECKS - totalNumberOfCardsDealt);
     }
 
-    private int getNumberOfValueCardsDealt(int value, List<Card> playerCards, List<Card> dealerCards) {
+    private int getNumberOfValueCardsDealt(int value, Hand playerHand, Hand dealerHand) {
+        List<Card> playerCards = playerHand.getCards();
+        List<Card> dealerCards = dealerHand.getCards();
         return (int) Stream.concat(playerCards.stream(), dealerCards.stream())
                 .filter(card -> card.rank().getValue() == value)
                 .count();
     }
 
-    private double calculateValueChanceTen(List<Card> playerCards, List<Card> dealerCards) {
-        int totalNumberOfCardsDealt = getTotalNumberOfCardsDealt(playerCards, dealerCards);
-        int numberOfValueCardsDealt = getNumberOfValueCardsDealt(10, playerCards, dealerCards);
+    private double calculateValueChanceTen(Hand playerHand, Hand dealerHand) {
+        int totalNumberOfCardsDealt = getTotalNumberOfCardsDealt(playerHand, dealerHand);
+        int numberOfValueCardsDealt = getNumberOfValueCardsDealt(10, playerHand, dealerHand);
         return (double) ((VALUE_TEN_CARDS_IN_CARD_DECK * NUMBER_OF_CARD_DECKS) - numberOfValueCardsDealt) /
                 ((NUMBER_OF_CARDS_IN_CARD_DECK * NUMBER_OF_CARD_DECKS) - totalNumberOfCardsDealt);
     }
 
-    private int getTotalNumberOfCardsDealt(List<Card> playerCards, List<Card> dealerCards) {
-        return playerCards.size() + dealerCards.size();
+    private int getTotalNumberOfCardsDealt(Hand playerHand, Hand dealerHand) {
+        return playerHand.getCards().size() + dealerHand.getCards().size();
     }
 
-    public int getPlayerHandTotal(List<Card> playerCards) {
-        return playerCards.stream().mapToInt(card -> card.rank().getValue()).sum();
-    }
-
-    private double calculateBustChance(int differenceToBlackjack, List<Card> playerCards, List<Card> dealerCards) {
+    private double calculateBustChance(int differenceToBlackjack, Hand playerHand, Hand dealerHand) {
         if (differenceToBlackjack >= 10) return 0.0;
-
         double bustChance = 0.0;
         for (int bustValue = differenceToBlackjack + 1; bustValue <= 10; bustValue++) {
-            bustChance += (bustValue == 10)
-                    ? calculateValueChanceTen(playerCards, dealerCards)
-                    : calculateValueChanceNotTen(bustValue, playerCards, dealerCards);
+            // Use hand.getTotal() with ace handling for a simulated next hand to avoid additional ambivalence handling
+            Card nextCard = new Card(Rank.fromValue(bustValue), Suit.CLUBS); // Choice of Suit irrelevant for calculation
+            Hand nextHand = new PlayerHand(playerHand.getCards());
+            nextHand.addCard(nextCard);
+            if (nextHand.getTotal() > 21) {
+                if (bustValue == 10) bustChance += calculateValueChanceTen(playerHand, dealerHand);
+                else bustChance += calculateValueChanceNotTen(bustValue, playerHand, dealerHand);
+            }
         }
         return bustChance;
     }
+
 }
