174c05b282cbb458f4bd8794574a63c48c9203e4
voelkerh <henriette.voelker@posteo.de>
2025-07-12 19:21:49 +0200

    Add javaDoc to hand interface

--- src/main/java/de/htwberlin/casino/blackjack/application/domain/model/Hand.java
+++ src/main/java/de/htwberlin/casino/blackjack/application/domain/model/Hand.java
@@ -4,9 +4,25 @@ import java.util.List;
 
 public interface Hand {
 
+    /**
+     * Adds card to the list of cards held in hand.
+     *
+     * @param card
+     */
     void addCard(Card card);
 
+    /**
+     * Returns a copy of the cards held in hand.
+     *
+     * @return List of cards held in hand
+     */
     List<Card> getCards();
 
+    /**
+     * Calculates total hand value.
+     * If hand contains an ACE and has total value > 21, ACE counts as 1 instead of 11.
+     *
+     * @return total hand value
+     */
     int getTotal();
 }
