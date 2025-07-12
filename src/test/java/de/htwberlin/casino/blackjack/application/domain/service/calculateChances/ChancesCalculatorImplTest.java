24c4ff4c8fbf6f0d9d85eed23bfb918e1f59e7c4
voelkerh <henriette.voelker@posteo.de>
2025-07-12 22:16:54 +0200

    Adjust Setup for ChancesCalculatorImplTest

--- src/test/java/de/htwberlin/casino/blackjack/application/domain/service/calculateChances/ChancesCalculatorImplTest.java
+++ src/test/java/de/htwberlin/casino/blackjack/application/domain/service/calculateChances/ChancesCalculatorImplTest.java
@@ -1,19 +1,26 @@
 package de.htwberlin.casino.blackjack.application.domain.service.calculateChances;
 
 import de.htwberlin.casino.blackjack.application.domain.model.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ChancesCalculatorImplTest {
 
+    DealerHand dealerHand;
+
+    @BeforeEach
+    void setUp() {
+        Card dealerUpCard = new Card(Rank.FIVE, Suit.SPADES);
+        dealerHand = new DealerHand(dealerUpCard);
+    }
+
     @Test
-    public void givenPlayerFiveTwoDealerThree_whenCalculateChances_thenReturnNotNull() {
+    public void givenPlayerFiveTwo_whenCalculateChances_thenReturnNotNull() {
         Card card1 = new Card(Rank.FIVE, Suit.CLUBS);
         Card card2 = new Card(Rank.TWO, Suit.CLUBS);
-        Card card3 = new Card(Rank.THREE, Suit.CLUBS);
         PlayerHand playerHand = new PlayerHand(card1, card2);
-        DealerHand dealerHand = new DealerHand(card3);
         ChancesCalculatorImpl calculator = new ChancesCalculatorImpl();
 
         Chances actual = calculator.calculateChances(playerHand, dealerHand);
@@ -22,20 +29,15 @@ class ChancesCalculatorImplTest {
     }
 
     @Test
-    public void givenPlayerFiveTwoDealerThree_whenCalculateChances_thenReturnCorrectChances() {
-        Card card1 = new Card(Rank.FIVE, Suit.CLUBS);
+    public void givenPlayerThreeTwo_whenCalculateChances_thenReturnValidBlackjackChances() {
+        Card card1 = new Card(Rank.THREE, Suit.CLUBS);
         Card card2 = new Card(Rank.TWO, Suit.CLUBS);
-        Card card3 = new Card(Rank.THREE, Suit.CLUBS);
         PlayerHand playerHand = new PlayerHand(card1, card2);
-        DealerHand dealerHand = new DealerHand(card3);
         ChancesCalculatorImpl calculator = new ChancesCalculatorImpl();
 
         Chances actual = calculator.calculateChances(playerHand, dealerHand);
-        double expectedBlackJack = 0.3;
-        double expectedBust = 0.5;
 
-        assertEquals(expectedBlackJack, actual.blackjack());
-        assertEquals(expectedBust, actual.bust());
+        assertTrue(actual.blackjack() < 0.1);
     }
 
 }
