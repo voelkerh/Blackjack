06d9abf5790f071275940eda8e98b19c26833cb1
voelkerh <henriette.voelker@posteo.de>
2025-07-12 21:02:38 +0200

    Controller: Add javaDoc, delete chancesResponse for simplicity, add 400 bad request for finished games

--- src/main/java/de/htwberlin/casino/blackjack/adapter/in/web/ChancesController.java
+++ src/main/java/de/htwberlin/casino/blackjack/adapter/in/web/ChancesController.java
@@ -1,7 +1,9 @@
 package de.htwberlin.casino.blackjack.adapter.in.web;
 
+import de.htwberlin.casino.blackjack.application.domain.service.calculateChances.Chances;
 import de.htwberlin.casino.blackjack.application.port.in.calculateChances.CalculateChancesCommand;
 import de.htwberlin.casino.blackjack.application.port.in.calculateChances.CalculateChancesUseCase;
+import de.htwberlin.casino.blackjack.application.port.in.emitRules.EmitRulesUseCase;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -14,6 +16,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * REST controller that exposes endpoints to calculate chances for next turn in ongoing blackjack game.
+ * Delegates to {@link CalculateChancesUseCase}.
+ */
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/blackjack/")
@@ -32,8 +38,9 @@ public class ChancesController {
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "Chances calculated successfully",
                     content = @Content(mediaType = "application/json",
-                            schema = @Schema(implementation = ChancesResponse.class))),
-            @ApiResponse(responseCode = "404", description = "Game not found", content = @Content)
+                            schema = @Schema(implementation = Chances.class))),
+            @ApiResponse(responseCode = "404", description = "Game not found", content = @Content),
+            @ApiResponse(responseCode = "400", description = "Bad request", content = @Content)
     })
     @GetMapping("/chances/{gameId}")
     public ResponseEntity<?> calculateChances(@PathVariable int gameId) {
