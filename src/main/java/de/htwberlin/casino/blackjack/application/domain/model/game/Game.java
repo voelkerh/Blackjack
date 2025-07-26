4d7269c98032b53c99b0d5283d163e0d53627034
Joh <132756126+JohPy@users.noreply.github.com>
2025-07-26 21:10:30 +0200

    Update Game Interface to include all necessary getters

--- src/main/java/de/htwberlin/casino/blackjack/application/domain/model/game/Game.java
+++ src/main/java/de/htwberlin/casino/blackjack/application/domain/model/game/Game.java
@@ -3,9 +3,13 @@ package de.htwberlin.casino.blackjack.application.domain.model.game;
 import de.htwberlin.casino.blackjack.application.domain.model.hands.Hand;
 
 public interface Game {
+    Long getGameId();
+
     GameState getGameState();
 
     Hand getPlayerHand();
 
     Hand getDealerHand();
+
+    double getBet();
 }
