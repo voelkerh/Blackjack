8670dc1c933cae1d949a1666807a7f1b7ec2a8cf
Joh <132756126+JohPy@users.noreply.github.com>
2025-07-26 13:41:32 +0200

    Update GameMapper to combine Hand mapping into one method

--- src/main/java/de/htwberlin/casino/blackjack/adapter/out/persistence/GameMapper.java
+++ src/main/java/de/htwberlin/casino/blackjack/adapter/out/persistence/GameMapper.java
@@ -1,46 +1,38 @@
 package de.htwberlin.casino.blackjack.adapter.out.persistence;
 
+import de.htwberlin.casino.blackjack.application.domain.factory.hands.HandFactory;
+import de.htwberlin.casino.blackjack.application.domain.factory.hands.HandFactoryImpl;
 import de.htwberlin.casino.blackjack.application.domain.model.cards.Card;
 import de.htwberlin.casino.blackjack.application.domain.model.cards.CardDeckImpl;
 import de.htwberlin.casino.blackjack.application.domain.model.cards.Rank;
 import de.htwberlin.casino.blackjack.application.domain.model.cards.Suit;
 import de.htwberlin.casino.blackjack.application.domain.model.game.GameImpl;
 import de.htwberlin.casino.blackjack.application.domain.model.game.GameState;
-import de.htwberlin.casino.blackjack.application.domain.model.hands.DealerHand;
-import de.htwberlin.casino.blackjack.application.domain.model.hands.PlayerHand;
+import de.htwberlin.casino.blackjack.application.domain.model.hands.Hand;
+import de.htwberlin.casino.blackjack.application.domain.model.hands.HandType;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
 public class GameMapper {
+    HandFactory factory = new HandFactoryImpl();
 
     public GameImpl mapToDomainEntity(GameJpaEntity gameJpaEntity, JpaDrawnCardsRepository drawnCardsRepository) {
-        return new GameImpl(gameJpaEntity.getId(), gameJpaEntity.getUserId(), mapToCardDeck(gameJpaEntity, drawnCardsRepository), mapToPlayerHand(gameJpaEntity), mapToDealerHand(gameJpaEntity), GameState.valueOf(gameJpaEntity.getGameState()), gameJpaEntity.getBet());
+        return new GameImpl(gameJpaEntity.getId(), gameJpaEntity.getUserId(), mapToCardDeck(gameJpaEntity, drawnCardsRepository), mapToHand(HandType.PLAYER, gameJpaEntity), mapToHand(HandType.DEALER, gameJpaEntity), GameState.valueOf(gameJpaEntity.getGameState()), gameJpaEntity.getBet());
     }
 
-    private PlayerHand mapToPlayerHand(GameJpaEntity gameJpaEntity) {
-        List<DrawnCardJpaEntity> playerHandCards = gameJpaEntity.getPlayerHand();
-        if (playerHandCards.size() < 2) {
-            throw new IllegalStateException("Invalid number of cards to map hands");
-        }
-        List<Card> cards = playerHandCards.stream()
+    private <T extends Hand> T mapToHand(HandType type, GameJpaEntity gameJpaEntity) {
+        List<DrawnCardJpaEntity> drawnCards = (type == HandType.DEALER) ? gameJpaEntity.getDealerHand() : gameJpaEntity.getPlayerHand();
+
+        List<Card> cards = drawnCards.stream()
                 .map(drawn -> {
                     CardJpaEntity c = drawn.getCardId();
                     return new Card(Rank.valueOf(c.getRank()), Suit.valueOf(c.getSuit()));
                 })
                 .toList();
-        return new PlayerHand(cards);
-    }
 
-    private DealerHand mapToDealerHand(GameJpaEntity gameJpaEntity) {
-        List<DrawnCardJpaEntity> dealerHandCard = gameJpaEntity.getDealerHand();
-        if (dealerHandCard.isEmpty()) {
-            throw new IllegalStateException("Invalid number of cards to map hands");
-        }
-        CardJpaEntity cardJpa = dealerHandCard.getFirst().getCardId();
-        Card card = new Card(Rank.valueOf(cardJpa.getRank()), Suit.valueOf(cardJpa.getSuit()));
-        return new DealerHand(card);
+        return factory.create(type, cards);
     }
 
     private CardDeckImpl mapToCardDeck(GameJpaEntity gameJpaEntity, JpaDrawnCardsRepository drawnCardsRepository) {
