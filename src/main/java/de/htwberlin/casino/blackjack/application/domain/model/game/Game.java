3b1a6fa04c1102efa69c1c271a5c88a695c8ee20
Joh <132756126+JohPy@users.noreply.github.com>
2025-07-26 22:21:09 +0200

    Add userId Getter to Game Interface

--- src/main/java/de/htwberlin/casino/blackjack/application/domain/model/game/Game.java
+++ src/main/java/de/htwberlin/casino/blackjack/application/domain/model/game/Game.java
@@ -5,6 +5,8 @@ import de.htwberlin.casino.blackjack.application.domain.model.hands.Hand;
 public interface Game {
     Long getGameId();
 
+    String getUserId();
+
     GameState getGameState();
 
     Hand getPlayerHand();
