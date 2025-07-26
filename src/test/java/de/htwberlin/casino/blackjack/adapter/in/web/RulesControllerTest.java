bbd118ebf65cd4b4516b59db042bd2b9cc1355af
voelkerh <henriette.voelker@posteo.de>
2025-07-26 21:43:41 +0200

    Add rules response and adjust controller and tests

--- src/test/java/de/htwberlin/casino/blackjack/adapter/in/web/RulesControllerTest.java
+++ src/test/java/de/htwberlin/casino/blackjack/adapter/in/web/RulesControllerTest.java
@@ -1,5 +1,6 @@
 package de.htwberlin.casino.blackjack.adapter.in.web;
 
+import de.htwberlin.casino.blackjack.application.domain.model.rules.RuleOption;
 import de.htwberlin.casino.blackjack.application.domain.model.rules.Rules;
 import de.htwberlin.casino.blackjack.application.port.in.emitRules.EmitRulesUseCase;
 import de.htwberlin.casino.blackjack.utility.ErrorWrapper;
@@ -26,36 +27,36 @@ class RulesControllerTest {
     @Test
     void VoidParameterForGeneral_WhenReadRules_ShouldReturnResponseEntity() {
         Rules rules = mock(Rules.class);
+        when(rules.option()).thenReturn(RuleOption.GENERAL);
         when(emitRulesUseCase.emitRules(any())).thenReturn(Result.success(rules));
 
         ResponseEntity<?> response = rulesController.readRules();
 
         assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertEquals(rules, response.getBody());
         verify(emitRulesUseCase, times(1)).emitRules(any());
     }
 
     @Test
     void ValidActionHit_WhenReadRules_ShouldReturnResponseEntity() {
         Rules rules = mock(Rules.class);
+        when(rules.option()).thenReturn(RuleOption.HIT);
         when(emitRulesUseCase.emitRules(any())).thenReturn(Result.success(rules));
 
         ResponseEntity<?> response = rulesController.readRules("HIT");
 
         assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertEquals(rules, response.getBody());
         verify(emitRulesUseCase, times(1)).emitRules(any());
     }
 
     @Test
     void ValidActionStand_WhenReadRules_ShouldReturnResponseEntity() {
         Rules rules = mock(Rules.class);
+        when(rules.option()).thenReturn(RuleOption.STAND);
         when(emitRulesUseCase.emitRules(any())).thenReturn(Result.success(rules));
 
         ResponseEntity<?> response = rulesController.readRules("STAND");
 
         assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertEquals(rules, response.getBody());
         verify(emitRulesUseCase, times(1)).emitRules(any());
     }
 
