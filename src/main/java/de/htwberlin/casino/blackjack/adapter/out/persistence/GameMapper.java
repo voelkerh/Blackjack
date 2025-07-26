07fd8f7a3e27e61b275fd2c3c2f2811852f53f6d
Joh <132756126+JohPy@users.noreply.github.com>
2025-07-26 12:47:29 +0200

    Refactor hand methods to GameMapper

--- src/main/java/de/htwberlin/casino/blackjack/adapter/out/persistence/GameMapper.java
+++ src/main/java/de/htwberlin/casino/blackjack/adapter/out/persistence/GameMapper.java
@@ -3,13 +3,37 @@ package de.htwberlin.casino.blackjack.adapter.out.persistence;
 import de.htwberlin.casino.blackjack.application.domain.model.*;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class GameMapper {
 
-    public Game mapToDomainEntity(Long gameId, Long userId, CardDeck cardDeck,
-                                  PlayerHand playerHand, DealerHand dealerHand, GameState gameState, double bet) {
-        return new Game(gameId, userId, cardDeck,
-                playerHand, dealerHand, gameState, bet);
+    public Game mapToDomainEntity(GameJpaEntity gameJpaEntity) {
+        return new Game(gameJpaEntity.getId(), gameJpaEntity.getUserId(), CardDeck.getInstance(), mapToPlayerHand(gameJpaEntity), mapToDealerHand(gameJpaEntity), GameState.valueOf(gameJpaEntity.getGameState()), gameJpaEntity.getBet());
+    }
+
+    private PlayerHand mapToPlayerHand(GameJpaEntity gameJpaEntity) {
+        List<DrawnCardJpaEntity> playerHandCards = gameJpaEntity.getPlayerHand();
+        if (playerHandCards.size() < 2) {
+            throw new IllegalStateException("Invalid number of cards to map hands");
+        }
+        List<Card> cards = playerHandCards.stream()
+                .map(drawn -> {
+                    CardJpaEntity c = drawn.getCardId();
+                    return new Card(Rank.valueOf(c.getRank()), Suit.valueOf(c.getSuit()));
+                })
+                .toList();
+        return new PlayerHand(cards);
+    }
+
+    private DealerHand mapToDealerHand(GameJpaEntity gameJpaEntity) {
+        List<DrawnCardJpaEntity> dealerHandCard = gameJpaEntity.getDealerHand();
+        if (dealerHandCard.isEmpty()) {
+            throw new IllegalStateException("Invalid number of cards to map hands");
+        }
+        CardJpaEntity cardJpa = dealerHandCard.getFirst().getCardId();
+        Card card = new Card(Rank.valueOf(cardJpa.getRank()), Suit.valueOf(cardJpa.getSuit()));
+        return new DealerHand(card);
     }
 
 }
--- src/main/java/de/htwberlin/casino/blackjack/adapter/out/persistence/JpaRepositoryAdapter.java
+++ src/main/java/de/htwberlin/casino/blackjack/adapter/out/persistence/JpaRepositoryAdapter.java
@@ -9,10 +9,7 @@ import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
 import java.util.List;
-
-import static org.antlr.v4.runtime.tree.xpath.XPath.findAll;
 
 @RequiredArgsConstructor
 @Repository
@@ -33,39 +30,23 @@ class JpaRepositoryAdapter implements LoadRulesPort, LoadStatsPort, LoadGamePort
     @Override
     public Game retrieveGame(Long gameId) {
         GameJpaEntity gameJpaEntity = gameRepository.findById(gameId).orElseThrow(EntityNotFoundException::new);
-        return gameMapper.mapToDomainEntity(gameJpaEntity.getId(), gameJpaEntity.getUserId(), getCardDeck(gameJpaEntity), getPlayerHand(gameJpaEntity), getDealerHand(gameJpaEntity), GameState.valueOf(gameJpaEntity.getGameState()), gameJpaEntity.getBet());
+        recreateCardDeck(gameJpaEntity);
+        return gameMapper.mapToDomainEntity(gameJpaEntity);
     }
 
-    private CardDeck getCardDeck(GameJpaEntity gameJpaEntity) {
+    private void recreateCardDeck(GameJpaEntity gameJpaEntity) {
         CardDeck cardDeck = CardDeck.getInstance();
 
         List<DrawnCardJpaEntity> drawnCardsJpa = drawnCardsRepository.findByGameId(gameJpaEntity);
 
         List<Card> drawnCards = drawnCardsJpa.stream()
-            .map(drawnCard -> {
-                CardJpaEntity cardJpa = drawnCard.getCardId();
-                return new Card(Rank.valueOf(cardJpa.getRank()), Suit.valueOf(cardJpa.getSuit()));
-            })
-            .toList();
+                .map(drawnCard -> {
+                    CardJpaEntity cardJpa = drawnCard.getCardId();
+                    return new Card(Rank.valueOf(cardJpa.getRank()), Suit.valueOf(cardJpa.getSuit()));
+                })
+                .toList();
 
         cardDeck.removeDealtCards(drawnCards);
-
-        return cardDeck;
     }
 
-    private PlayerHand getPlayerHand(GameJpaEntity gameJpaEntity) {
-        List<DrawnCardJpaEntity> playerHandCards = gameJpaEntity.getPlayerHand();
-        CardJpaEntity card1Jpa = playerHandCards.getFirst().getCardId();
-        Card card1 = new Card(Rank.valueOf(card1Jpa.getRank()), Suit.valueOf(card1Jpa.getSuit()));
-        CardJpaEntity card2Jpa = playerHandCards.get(1).getCardId();
-        Card card2 = new Card(Rank.valueOf(card2Jpa.getRank()), Suit.valueOf(card2Jpa.getSuit()));
-        return new PlayerHand(card1, card2);
-    }
-
-    private DealerHand getDealerHand(GameJpaEntity gameJpaEntity) {
-        List<DrawnCardJpaEntity> dealerHandCard = gameJpaEntity.getDealerHand();
-        CardJpaEntity cardJpa = dealerHandCard.getFirst().getCardId();
-        Card card = new Card(Rank.valueOf(cardJpa.getRank()), Suit.valueOf(cardJpa.getSuit()));
-        return new DealerHand(card);
-    }
 }
