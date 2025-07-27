f61d68e05ae272f5a66330f81d6c7ab4373a1e95
Joh <132756126+JohPy@users.noreply.github.com>
2025-07-27 18:12:24 +0200

    Update HandFactoryImpl to allow DealerHand with more than 1 hand to be recreated

--- src/main/java/de/htwberlin/casino/blackjack/application/domain/factory/hands/HandFactoryImpl.java
+++ src/main/java/de/htwberlin/casino/blackjack/application/domain/factory/hands/HandFactoryImpl.java
@@ -11,7 +11,7 @@ import java.util.List;
 /**
  * A concrete implementation of hand factory methods.
  */
-public class HandFactoryImpl implements  HandFactory {
+public class HandFactoryImpl implements HandFactory {
 
     @Override
     @SuppressWarnings("unchecked") // type casting is done on purpose and controlled
@@ -19,10 +19,17 @@ public class HandFactoryImpl implements  HandFactory {
         if (cards == null || type == null) throw new IllegalArgumentException("Parameters cannot be null");
         switch (type) {
             case DEALER:
-                if (cards.size() != 1) throw new IllegalArgumentException("Dealerhand can only be initialized with one card");
-                return (T) new DealerHand(cards.get(0));
+                if (cards.isEmpty()) {
+                    throw new IllegalArgumentException("DealerHand must be initialized with at least one card");
+                }
+                DealerHand dealerHand = new DealerHand(cards.getFirst());
+                for (int i = 1; i < cards.size(); i++) {
+                    dealerHand.addCard(cards.get(i));
+                }
+                return (T) dealerHand;
             case PLAYER:
-                if (cards.size() < 2) throw new IllegalArgumentException("Playerhand can only be initialized with more than one card");
+                if (cards.size() < 2)
+                    throw new IllegalArgumentException("Playerhand can only be initialized with more than one card");
                 else if (cards.size() == 2) return (T) new PlayerHand(cards.get(0), cards.get(1));
                 PlayerHand playerHand = new PlayerHand(cards.get(0), cards.get(1));
                 for (int i = 2; i < cards.size(); i++) {
