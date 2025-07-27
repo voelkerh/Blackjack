68678ad85ca20ed3951615438d24f0d5c9696a61
Joh <132756126+JohPy@users.noreply.github.com>
2025-07-27 15:00:12 +0200

    Add port.in doc

--- src/main/java/de/htwberlin/casino/blackjack/application/port/in/emitRules/EmitRulesQuery.java
+++ src/main/java/de/htwberlin/casino/blackjack/application/port/in/emitRules/EmitRulesQuery.java
@@ -2,5 +2,10 @@ package de.htwberlin.casino.blackjack.application.port.in.emitRules;
 
 import de.htwberlin.casino.blackjack.application.domain.model.rules.RuleOption;
 
+/**
+ * Query to retrieve the rules associated with a specific {@link RuleOption}.
+ *
+ * @param option the {@link RuleOption} for which the rules should be emitted.
+ */
 public record EmitRulesQuery(RuleOption option) {
 }
