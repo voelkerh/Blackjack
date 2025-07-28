97bca7ea2ee009becba34cc3b1e16bcc4589cec4
Joh <132756126+JohPy@users.noreply.github.com>
2025-07-28 14:50:16 +0200

    Update queries in JpaGameRepository to fetch simpler data
    * Update JpaRepositoryAdapter accordingly

--- src/main/java/de/htwberlin/casino/blackjack/adapter/out/persistence/JpaGameRepository.java
+++ src/main/java/de/htwberlin/casino/blackjack/adapter/out/persistence/JpaGameRepository.java
@@ -1,8 +1,6 @@
 package de.htwberlin.casino.blackjack.adapter.out.persistence;
 
 import de.htwberlin.casino.blackjack.application.domain.model.game.GameState;
-import de.htwberlin.casino.blackjack.application.domain.service.emitStats.OverviewStats;
-import de.htwberlin.casino.blackjack.application.domain.service.emitStats.UserStats;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -17,41 +15,48 @@ public interface JpaGameRepository extends JpaRepository<GameJpaEntity, Long> {
     @Query("SELECT g FROM game g LEFT JOIN FETCH g.drawnCards WHERE g.id = :id")
     Optional<GameJpaEntity> findByIdWithDrawnCards(@Param("id") Long id);
 
+    @Query("SELECT COUNT(g) FROM game g")
+    Long fetchTotalGames();
+
+    @Query("SELECT COUNT(DISTINCT g.userId) FROM game g")
+    Long fetchTotalPlayers();
+
+    @Query("SELECT COALESCE(SUM(g.bet), 0) FROM game g")
+    Double fetchTotalBet();
+
+    @Query(value = """
+            SELECT COALESCE(SUM(
+                CASE
+                    WHEN game_state = 'WIN' THEN -bet
+                    WHEN game_state = 'BLACKJACK' THEN -bet * 1.5
+                    WHEN game_state = 'LOSS' THEN bet
+                    ELSE 0
+                END
+            ), 0)
+            FROM game
+            """, nativeQuery = true)
+    Double fetchHouseProfit();
+
     @Query("SELECT COUNT(g) FROM game g WHERE g.userId = :userId")
     Long fetchNumberOfGamesPlayedByUser(@Param("userId") String userId);
 
-    @Query("SELECT SUM(CASE WHEN g.gameState = :gameState THEN 1 ELSE 0 END) FROM game g WHERE g.userId = :userId")
-    int fetchNumberOfGamesWithGameSateOfUser(@Param("userId") String userId, @Param("gameSate") GameState gameState);
+    @Query("SELECT COUNT(g) FROM game g WHERE g.userId = :userId AND g.gameState = :gameState")
+    Long fetchNumberOfGamesWithGameStateOfUser(@Param("userId") String userId, @Param("gameState") GameState gameState);
 
     @Query("SELECT COALESCE(SUM(g.bet), 0) FROM game g WHERE g.userId = :userId")
-    int fetchTotalBetByUser(@Param("userId") String userId);
+    Double fetchTotalBetByUser(@Param("userId") String userId);
 
-    @Query("""
-                SELECT new de.htwberlin.casino.blackjack.application.domain.service.emitStats.UserStats(
-                    COUNT(g),
-                            CONCAT(
-                                SUM(CASE WHEN g.gameState = 'WON' THEN 1 ELSE 0 END), ':',
-                                SUM(CASE WHEN g.gameState = 'LOST' THEN 1 ELSE 0 END), ':',
-                                SUM(CASE WHEN g.gameState = 'PUSH' THEN 1 ELSE 0 END)
-                            ),
-                            COALESCE(SUM(g.bet), 0),
-                            COALESCE(SUM(CASE WHEN g.gameState = 'WON' THEN g.bet ELSE 0 END), 0) -
-                            COALESCE(SUM(CASE WHEN g.gameState = 'LOST' THEN g.bet ELSE 0 END), 0)
-                        )
-                FROM game g
-                WHERE g.userId = :userId
-            """)
-    UserStats fetchUserStats(@Param("userId") String userId);
-
-    @Query("""
-                SELECT new de.htwberlin.casino.blackjack.application.domain.service.emitStats.OverviewStats(
-                    COUNT(g),
-                    COUNT(DISTINCT g.userId),
-                    COALESCE(SUM(g.bet), 0),
-                    COALESCE(SUM(CASE WHEN g.gameState = 'LOST' THEN g.bet ELSE 0 END), 0) -
-                    COALESCE(SUM(CASE WHEN g.gameState = 'WON' THEN g.bet ELSE 0 END), 0)
-                )
-                FROM game g
-            """)
-    OverviewStats fetchOverviewStats();
+    @Query(value = """
+            SELECT COALESCE(SUM(
+                CASE
+                    WHEN game_state = 'WIN' THEN bet * 2
+                    WHEN game_state = 'BLACKJACK' THEN bet * 2.5
+                    WHEN game_state = 'PUSH' THEN bet
+                    ELSE 0
+                END
+            ), 0)
+            FROM game
+            WHERE user_id = :userId
+            """, nativeQuery = true)
+    Double fetchNetResultByUser(@Param("userId") String userId);
 }
