4eb914136bd071b4edd81dd393fc99951708d926
Joh <132756126+JohPy@users.noreply.github.com>
2025-07-28 13:11:41 +0200

    Add queries to JpaGameRepository necessary for StatsCalculator

--- src/main/java/de/htwberlin/casino/blackjack/adapter/out/persistence/JpaGameRepository.java
+++ src/main/java/de/htwberlin/casino/blackjack/adapter/out/persistence/JpaGameRepository.java
@@ -1,5 +1,6 @@
 package de.htwberlin.casino.blackjack.adapter.out.persistence;
 
+import de.htwberlin.casino.blackjack.application.domain.model.game.GameState;
 import de.htwberlin.casino.blackjack.application.domain.service.emitStats.OverviewStats;
 import de.htwberlin.casino.blackjack.application.domain.service.emitStats.UserStats;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -15,6 +16,15 @@ public interface JpaGameRepository extends JpaRepository<GameJpaEntity, Long> {
 
     @Query("SELECT g FROM game g LEFT JOIN FETCH g.drawnCards WHERE g.id = :id")
     Optional<GameJpaEntity> findByIdWithDrawnCards(@Param("id") Long id);
+
+    @Query("SELECT COUNT(g) FROM game g WHERE g.userId = :userId")
+    Long fetchNumberOfGamesPlayedByUser(@Param("userId") String userId);
+
+    @Query("SELECT SUM(CASE WHEN g.gameState = :gameState THEN 1 ELSE 0 END) FROM game g WHERE g.userId = :userId")
+    int fetchNumberOfGamesWithGameSateOfUser(@Param("userId") String userId, @Param("gameSate") GameState gameState);
+
+    @Query("SELECT COALESCE(SUM(g.bet), 0) FROM game g WHERE g.userId = :userId")
+    int fetchTotalBetByUser(@Param("userId") String userId);
 
     @Query("""
                 SELECT new de.htwberlin.casino.blackjack.application.domain.service.emitStats.UserStats(
